c6b4c861e0f0151e723d015fcf30f3bd98b60912
Rahma Shahbaz <samisk3232@gmail.com>
2025-11-13 20:04:10 -0800

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum Cost to Merge Stones/minimum-cost-to-merge-stones.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    int mergeStones(vector<int> &stones, int k) {
+        int n = stones.size();
+        if ((n - 1) % (k - 1) != 0)
+            return -1;
+            
+        // Prefix sum for fast range sum
+        vector<int> prefix(n + 1, 0);
+        for (int i = 0; i < n; i++)
+            prefix[i + 1] = prefix[i] + stones[i];
+
+        const int INF = 1e9;
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(n, vector<int>(k + 1, INF))
+        );
+        for (int i = 0; i < n; i++)
+            dp[i][i][1] = 0;
+
+        // Interval DP
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                // Merge into t piles
+                for (int t = 2; t <= k; t++) {
+                    for (int mid = i; mid < j; mid += (k - 1)) {
+                        if (dp[i][mid][1] < INF && dp[mid + 1][j][t - 1] < INF) {
+                            dp[i][j][t] = min(dp[i][j][t],
+                                dp[i][mid][1] + dp[mid + 1][j][t - 1]
+                            );
+                        }
+                    }
+                }
+
+                // If we can make k piles, merge them into 1 pile
+                if (dp[i][j][k] < INF)
+                    dp[i][j][1] = dp[i][j][k] + (prefix[j + 1] - prefix[i]);
+            }
+        }
+
+        return dp[0][n - 1][1];
+    }
+};
